616199977709d00e7d4c9a03babb5605c3863526
duyluc <duongduyluc3@gmail.com>
2022-05-10 08:38:39 +0700

    Add project files.

---
+++ EditModelTable.Ver1/EditModelTable.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace EditModelTable.Ver1
+{
+    partial class EditModelTable
+    {
+
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Datagridview = new System.Windows.Forms.DataGridView();
+            this.PropertyName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Datagridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Datagridview
+            // 
+            this.Datagridview.AllowUserToAddRows = false;
+            this.Datagridview.AllowUserToDeleteRows = false;
+            this.Datagridview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Datagridview.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.Datagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Datagridview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PropertyName,
+            this.Value});
+            this.Datagridview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Datagridview.GridColor = System.Drawing.Color.Linen;
+            this.Datagridview.Location = new System.Drawing.Point(0, 0);
+            this.Datagridview.Name = "Datagridview";
+            this.Datagridview.Size = new System.Drawing.Size(492, 461);
+            this.Datagridview.TabIndex = 0;
+            this.Datagridview.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.Datagridview_CellValueChanged);
+            // 
+            // PropertyName
+            // 
+            this.PropertyName.FillWeight = 91.37056F;
+            this.PropertyName.HeaderText = "Property Name";
+            this.PropertyName.MinimumWidth = 80;
+            this.PropertyName.Name = "PropertyName";
+            this.PropertyName.ReadOnly = true;
+            this.PropertyName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Value
+            // 
+            this.Value.FillWeight = 108.6294F;
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // EditModelTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Datagridview);
+            this.Name = "EditModelTable";
+            this.Size = new System.Drawing.Size(492, 461);
+            ((System.ComponentModel.ISupportInitialize)(this.Datagridview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridViewTextBoxColumn PropertyName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridView Datagridview;
+    }
+}
---
+++ EditModelTable.Ver1/EditModelTable.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace EditModelTable.Ver1
+{
+    public partial class EditModelTable: UserControl
+    {
+        private PropertyInfo[] PropertyInfo { get; set; }
+        private Type ModelType { get; set; }
+        public EditModelTable()
+        {
+            InitializeComponent();
+            this.Datagridview.Columns[0].Width = 80;
+        }
+
+        public void SetModel(Type _modelType)
+        {
+            this.ModelType = _modelType;
+            this.PropertyInfo = _modelType.GetProperties();
+            foreach (PropertyInfo p in this.PropertyInfo)
+            {
+                DataGridViewRow newrow = new DataGridViewRow();
+                DataGridViewTextBoxCell namecell = new DataGridViewTextBoxCell();
+                namecell.Value = p.Name;
+                DataGridViewCell valuecell = null;
+                if (p.PropertyType == typeof(bool))
+                {
+                    valuecell = new DataGridViewCheckBoxCell();
+                    valuecell.Value = false;
+                }
+                else
+                {
+                    valuecell = new DataGridViewTextBoxCell();
+                    valuecell.Value = null;
+                }
+                valuecell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                newrow.Cells.Add(namecell);
+                newrow.Cells.Add(valuecell);
+                this.Datagridview.Rows.Add(newrow);
+            }
+        }
+
+        public Dictionary<string,object> GetModelProperties()
+        {
+            Dictionary<string, object> propertiesValue = new Dictionary<string, object>();
+            int i = 0;
+            foreach(PropertyInfo p in PropertyInfo)
+            {
+                object propertyvalue = this.Datagridview.Rows[i].Cells[1].Value;
+                if (propertyvalue == null) throw new ArgumentNullException();
+                if(p.PropertyType != typeof(bool))
+                {
+                    string value = (string)(propertyvalue);
+                    if(p.PropertyType == typeof(string))
+                    {
+                        propertyvalue = value;
+                    }
+                    if (p.PropertyType == typeof(int))
+                    {
+                        propertyvalue = int.Parse(value);
+                    }
+                    if (p.PropertyType == typeof(Int16))
+                    {
+                        propertyvalue = Int16.Parse(value);
+                    }
+                    if (p.PropertyType == typeof(Int64))
+                    {
+                        propertyvalue = Int64.Parse(value);
+                    }
+                    if (p.PropertyType == typeof(float))
+                    {
+                        propertyvalue = float.Parse(value);
+                    }
+                    if (p.PropertyType == typeof(double))
+                    {
+                        propertyvalue = double.Parse(value);
+                    }
+                }
+                propertiesValue.Add(p.Name, propertyvalue);
+                i++;
+            }
+            return propertiesValue;
+        }
+        private void Datagridview_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex != 1) return;
+            int rowindex = e.RowIndex;
+            if (rowindex < 0) return;
+            if (this.Datagridview.Rows[rowindex].Cells[1].Value == null) return;
+            if (this.Datagridview.Rows[rowindex].Cells[1].FormattedValueType == typeof(bool)) return;
+            string value = (string)this.Datagridview.Rows[rowindex].Cells[1].Value;
+            Type celltype = this.PropertyInfo[rowindex].PropertyType;
+            try
+            {
+                if (celltype == typeof(int))
+                {
+                    if (!int.TryParse(value, out int _t)) throw new Exception();
+                }
+                if (celltype == typeof(Int16))
+                {
+                    if (!Int16.TryParse(value, out Int16 _t)) throw new Exception();
+                }
+                if (celltype == typeof(Int64))
+                {
+                    if (!Int64.TryParse(value, out Int64 _t)) throw new Exception();
+                }
+                if (celltype == typeof(float))
+                {
+                    if (!float.TryParse(value, out float _t)) throw new Exception();
+                }
+                if (celltype == typeof(double))
+                {
+                    if (!double.TryParse(value, out double _t)) throw new Exception();
+                }
+            }
+            catch
+            {
+                this.Datagridview.Rows[rowindex].Cells[1].Value = null;
+                MessageBox.Show("Value is Invalid!");
+            }
+        }
+    }
+}
