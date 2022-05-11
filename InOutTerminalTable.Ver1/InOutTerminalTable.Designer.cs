2838d47cef458bca43e8b9883977dc17366937d3
duyluc <duongduyluc3@gmail.com>
2022-05-11 16:51:33 +0700

    Create InOutTerminalTable

---
+++ InOutTerminalTable.Ver1/InOutTerminalTable.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace InOutTerminalTable.Ver1
+{
+    partial class InOutTerminalTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Datagridview = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Datagridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Datagridview
+            // 
+            this.Datagridview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Datagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Datagridview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Type,
+            this.Value});
+            this.Datagridview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Datagridview.Location = new System.Drawing.Point(0, 0);
+            this.Datagridview.Name = "Datagridview";
+            this.Datagridview.RowHeadersWidth = 62;
+            this.Datagridview.RowTemplate.Height = 24;
+            this.Datagridview.Size = new System.Drawing.Size(487, 552);
+            this.Datagridview.TabIndex = 0;
+            // 
+            // Name
+            // 
+            this.Name.FillWeight = 85.22727F;
+            this.Name.HeaderText = "Name";
+            this.Name.MinimumWidth = 150;
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            // 
+            // Type
+            // 
+            this.Type.FillWeight = 107.3864F;
+            this.Type.HeaderText = "Type";
+            this.Type.MinimumWidth = 50;
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            // 
+            // Value
+            // 
+            this.Value.FillWeight = 107.3864F;
+            this.Value.HeaderText = "Value";
+            this.Value.MinimumWidth = 8;
+            this.Value.Name = "Value";
+            // 
+            // InOutTerminalTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Datagridview);
+            this.Size = new System.Drawing.Size(487, 552);
+            ((System.ComponentModel.ISupportInitialize)(this.Datagridview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Datagridview;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
---
+++ InOutTerminalTable.Ver1/InOutTerminalTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VisionSupport;
+
+namespace InOutTerminalTable.Ver1
+{
+    public partial class InOutTerminalTable: UserControl
+    {
+        public InOutTerminalTable()
+        {
+            InitializeComponent();
+        }
+
+        public void SetValue2View(Dictionary<string,Terminal> inOutTerminal)
+        {
+            if (inOutTerminal == null) throw new ArgumentNullException();
+            this.Datagridview.Invoke(new Action(() =>
+            {
+                this.Datagridview.Rows.Clear();
+            }));
+            foreach (KeyValuePair<string, Terminal> Ter in inOutTerminal)
+            {
+                Terminal terminal = Ter.Value;
+                DataGridViewRow newrow = new DataGridViewRow();
+                DataGridViewTextBoxCell namecell = new DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell typecell = new DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell valuecell = new DataGridViewTextBoxCell();
+                namecell.Value = terminal.Name;
+                if (terminal.Type == typeof(Bitmap))
+                {
+                    typecell.Value = "ICogImage";
+                    valuecell.Value = "ICogImage";
+                }
+                else
+                {
+                    typecell.Value = terminal.Type.ToString();
+                    valuecell.Value = Convert.ChangeType(terminal.Value,terminal.Type);
+                }
+                newrow.Cells.AddRange(new DataGridViewCell[3] { namecell, typecell, valuecell });
+                this.Datagridview.Invoke(new Action(() =>
+                {
+                    this.Datagridview.Rows.Add(newrow);
+                }));
+            }
+            
+        }
+    }
+}
